1caaef0a1aca44ca08c0fa8784f6d73eee5d411c
DevYouriD <youri.dera@gmail.com>
2022-11-01 15:17:01 +0100

    Some small improvements

--- src/test/java/com/example/exampleapp/ApplicationTests.java
+++ src/test/java/com/example/exampleapp/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ExampleAppApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
